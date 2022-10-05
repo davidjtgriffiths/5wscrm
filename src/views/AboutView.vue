98bb764c7e9819ffd1139ea02700523b019b31d4
David Griffiths <davidgriffiths@hotmail.com>
2022-10-05 14:28:19 +0100

    dashboard

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,33 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <va-avatar>
+    <va-icon name="warning" />
+  </va-avatar>
+  <va-button> Button </va-button>
+  <div style="height: 13rem; border: 1px solid var(--va-secondary);">
+    <va-sidebar textColor="dark">
+      <va-sidebar-item>
+        <va-sidebar-item-content>
+          <va-icon name="dashboard" />
+          <!-- User can hide item with css if he wants -->
+          <va-sidebar-item-title>Dashboard</va-sidebar-item-title>
+        </va-sidebar-item-content>
+      </va-sidebar-item>
+
+      <va-sidebar-item active>
+        <va-sidebar-item-content>
+          <va-icon name="room" />
+          <va-sidebar-item-title>Sidebar demo</va-sidebar-item-title>
+        </va-sidebar-item-content>
+      </va-sidebar-item>
+
+      <va-sidebar-item>
+        <va-sidebar-item-content>
+          <va-icon name="loop" />
+          <va-sidebar-item-title>Loop</va-sidebar-item-title>
+        </va-sidebar-item-content>
+      </va-sidebar-item>
+    </va-sidebar>
+  </div>
 </template>
