75362053bac65bd50fc276f7e1b65c6d09707c8b
David Griffiths <davidgriffiths@hotmail.com>
2022-10-12 16:01:31 +0100

    linked dropdowns and lead table

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import './styles/app.css';
 import { createVuestic } from 'vuestic-ui'
 import "vuestic-ui/styles/essential.css";
 import "vuestic-ui/styles/grid.css";
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,18 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-  <va-avatar>
-    <va-icon name="warning" />
-  </va-avatar>
-  <va-button> Button </va-button>
-  <div style="height: 13rem; border: 1px solid var(--va-secondary);">
+
+ <template>
+  <va-navbar>
+    <template #left>
+      <va-navbar-item>Left</va-navbar-item>
+    </template>
+    <template #center>
+      <va-navbar-item>Center</va-navbar-item>
+    </template>
+    <template #right>
+      <va-navbar-item><va-avatar src="https://randomuser.me/api/portraits/women/5.jpg" /></va-navbar-item>
+    </template>
+  </va-navbar>
+  <div class="row">
+  <div class="column" style="border: 1px solid var(--va-secondary);">
     <va-sidebar textColor="dark">
       <va-sidebar-item>
         <va-sidebar-item-content>
@@ -18,24 +24,126 @@
 
       <va-sidebar-item active>
         <va-sidebar-item-content>
-          <va-icon name="room" />
-          <va-sidebar-item-title>Sidebar demo</va-sidebar-item-title>
+          <va-icon name="request_quote" />
+          <va-sidebar-item-title>Quote</va-sidebar-item-title>
+        </va-sidebar-item-content>
+      </va-sidebar-item>
+
+
+      <va-sidebar-item>
+        <va-sidebar-item-content>
+          <va-icon name="add_location" />
+          <va-sidebar-item-title>Survey</va-sidebar-item-title>
+        </va-sidebar-item-content>
+      </va-sidebar-item>
+
+      <va-sidebar-item>
+        <va-sidebar-item-content>
+          <va-icon name="photo_camera" />
+          <va-sidebar-item-title>Inspection</va-sidebar-item-title>
         </va-sidebar-item-content>
       </va-sidebar-item>
 <!-- Hello tony! -->
+
       <va-sidebar-item>
         <va-sidebar-item-content>
-          <va-icon name="loop" />
-          <va-sidebar-item-title>Loop</va-sidebar-item-title>
+          <va-icon name="inventory" />
+          <va-sidebar-item-title>Products</va-sidebar-item-title>
         </va-sidebar-item-content>
       </va-sidebar-item>
+
+      <va-sidebar-item>
+        <va-sidebar-item-content>
+          <va-icon name="payments" />
+          <va-sidebar-item-title>Sales</va-sidebar-item-title>
+        </va-sidebar-item-content>
+      </va-sidebar-item>
+
+      <va-sidebar-item>
+        <va-sidebar-item-content>
+          <va-icon name="settings" />
+          <va-sidebar-item-title>Settings</va-sidebar-item-title>
+        </va-sidebar-item-content>
+      </va-sidebar-item>
+
+
     </va-sidebar>
+
   </div>
-  <va-collapse
-    v-model="value"
-    style="width: 400px;"
-    header="Collapse header"
-  >
-    <div>Collapse content</div>
-  </va-collapse>
+  <div class="column mt-6">
+  <div class="row-span-3">
+    <va-select class="mb-4" label="Default" v-model="value" :options="options" />
+    <va-select class="mb-4" label="Price" v-model="value" :options="options" text-by="price" />
+
+    <va-input
+      class="mb-4"
+      v-model="actual"
+      :value="value.price"
+    />
+
+<va-input class="mb-4">{{ (actual - value.price) }}</va-input>
+  </div>
+
+  <va-form style="width: 300px;">
+    <va-select class="mb-4" label="Default" v-model="value" :options="options" />
+
+    <va-select class="mb-4" label="Text by" v-model="value" :options="options" text-by="textBy" />
+
+    <va-select class="mb-4" label="Text by (function)" v-model="value" :options="options" :text-by="(option) => option.textBy" />
+
+    <va-select class="mb-4" label="value by" v-model="value" :options="options" value-by="valueBy" />
+
+    <va-select class="mb-4" label="value by (function)" v-model="value" :options="options" :value-by="(option) => option.valueBy" />
+  </va-form>
+</div>
+<div class="column mt-6">
+
+<va-form style="width: 300px;">
+  <va-select class="mb-4" label="Default" v-model="value" :options="options" />
+
+  <va-select class="mb-4" label="Text by" v-model="value" :options="options" text-by="textBy" />
+
+  <va-select class="mb-4" label="Text by (function)" v-model="value" :options="options" :text-by="(option) => option.textBy" />
+
+  <va-select class="mb-4" label="value by" v-model="value" :options="options" value-by="valueBy" />
+
+  <va-select class="mb-4" label="value by (function)" v-model="value" :options="options" :value-by="(option) => option.valueBy" />
+</va-form>
+</div>
+</div>
+
 </template>
+
+<script setup>
+
+import { ref, onMounted } from 'vue'
+
+const options = [
+      {
+        text: 'First',
+        textBy: 'First text by',
+        value: '1',
+        valueBy: 'first',
+        price: 100,
+      },
+      {
+        text: 'Second',
+        textBy: 'Second text by',
+        value: '2',
+        valueBy: 'second',
+        price: 500,
+      },
+      {
+        text: 'Third',
+        textBy: 'Third text by',
+        value: '3',
+        valueBy: 'third',
+        price: 200,
+      },
+    ]
+   const props = defineProps(['value'])
+const value = ref(options[0])
+const actual = ref(null)
+
+
+</script>
